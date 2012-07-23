5a22f900de4db49025f427065462f65f31013f28
Alan Malloy <alan@malloys.org>
2012-07-23 12:18:03 -0700

    There's a primitive version of rem, but not mod

--- src/ring_buffer/core.clj
+++ src/ring_buffer/core.clj
@@ -19,11 +19,11 @@
 
   IPersistentStack
   (peek [this]
-    (nth buf (mod start (count buf))))
+    (nth buf (rem start (count buf))))
   (pop [this]
     (if (zero? len)
       (throw (IllegalStateException. "Can't pop empty queue"))
-      (RingBuffer. (mod (inc start) (count buf)) (dec len) (assoc buf start nil) meta)))
+      (RingBuffer. (rem (inc start) (count buf)) (dec len) (assoc buf start nil) meta)))
   (empty [this]
     (RingBuffer. 0 0 (vec (repeat (count buf) nil)) meta))
   (equiv [this other]
@@ -35,11 +35,11 @@
   IPersistentCollection
   (cons [this x]
     (if (= len (count buf))
-      (RingBuffer. (mod (inc start) len) len (assoc buf start x) meta)
-      (RingBuffer. start (inc len) (assoc buf (mod (+ start len) (count buf)) x) meta)))
+      (RingBuffer. (rem (inc start) len) len (assoc buf start x) meta)
+      (RingBuffer. start (inc len) (assoc buf (rem (+ start len) (count buf)) x) meta)))
   (seq [this]
     (seq (for [i (range len)]
-           (nth buf (mod (+ start i) (count buf)))))))
+           (nth buf (rem (+ start i) (count buf)))))))
 
 (defmethod print-method RingBuffer [b ^Writer w]
   (.write w "(")
