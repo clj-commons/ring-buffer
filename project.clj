40daba58cab239dbe90bc6ef63fe9c31efb725ce
micahelrkytch <michaelr.kytch@gmail.com>
2015-04-16 15:15:23 -0700

    Implement java.util.Collection required methods

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject amalloy/ring-buffer "1.0"
+(defproject amalloy/ring-buffer "1.1"
   :description "Persistent bounded-size queue implementation in Clojure"
   :url "http://github.com/amalloy/ring-buffer"
   :license {:name "Eclipse Public License"
