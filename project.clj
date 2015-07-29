2cc9f2132ee61bdf77fd3315bf324228eced84e7
Alan Malloy <alan@malloys.org>
2015-07-29 10:46:51 -0700

    Add a c alias to avoid typing all this junk

--- project.clj
+++ project.clj
@@ -7,4 +7,5 @@
   :aot :all
   :profiles {:skummet {:exclusions [[org.clojure/clojure]]
                        :dependencies [[org.skummet/clojure "1.7.0-RC1-r4"]]
-                       :plugins [[org.skummet/lein-skummet "0.2.1"]]}})
+                       :plugins [[org.skummet/lein-skummet "0.2.1"]]}}
+  :aliases {"c" ["do" "clean," "with-profile" "skummet" "skummet" "compile"]})
