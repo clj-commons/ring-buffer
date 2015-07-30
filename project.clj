733ba7d682bc683ae67a2e8ef5f27ba808a17d62
Alan Malloy <alan@malloys.org>
2015-07-30 10:28:30 -0700

    Remove profiles entirely (for the moment)

--- project.clj
+++ project.clj
@@ -3,17 +3,16 @@
   :url "http://github.com/amalloy/ring-buffer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]]
+  ;:dependencies [[org.clojure/clojure "1.7.0"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :aot :all
   :main amalloy.ring-buffer
   :javac-options ["-target" "1.7" "-source" "1.7"]
-  :profiles {:skummet {:exclusions [[org.clojure/clojure]]
+  :exclusions [[org.clojure/clojure]]
                        :plugins [[org.skummet/lein-skummet "0.2.1"]]
-                       :dependencies ^:replace [[com.factual.skummet/clojure "1.7.0-RC1-r4"]]}
-             ;:default []
-             }
+                       :dependencies ^:replace [[com.factual.skummet/clojure "1.7.0-RC1-r4"]]
+  :profiles {:default []}
 
   :aliases {"c" ["with-profile" "skummet" "do" "clean," "skummet" "compile"]
             "j" ["with-profile" "skummet" "do" "clean," "skummet" "jar"]})
