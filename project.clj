5d688c0abe67014b1630672865c732d25438f912
Alan Malloy <alan@malloys.org>
2015-07-30 11:31:33 -0700

    Use ^:displace instead of ^:replace
    These should be equivalent, but there is some kind of weird
    interaction between lein profiles and skummet so they're not?

--- project.clj
+++ project.clj
@@ -3,16 +3,17 @@
   :url "http://github.com/amalloy/ring-buffer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  ;:dependencies [[org.clojure/clojure "1.7.0"]]
+  :dependencies ^:displace [[org.clojure/clojure "1.7.0"]]
   :source-paths ["src/clj"]
   :java-source-paths ["src/java"]
   :aot :all
   :main amalloy.ring-buffer
   :javac-options ["-target" "1.7" "-source" "1.7"]
-  :exclusions [[org.clojure/clojure]]
+  :profiles {:skummet {:exclusions [[org.clojure/clojure]]
                        :plugins [[org.skummet/lein-skummet "0.2.1"]]
-                       :dependencies ^:replace [[com.factual.skummet/clojure "1.7.0-RC1-r4"]]
-  :profiles {:default []}
+                       :dependencies [[com.factual.skummet/clojure "1.7.0-RC1-r4"]]}
+             ;:default []
+             }
 
   :aliases {"c" ["with-profile" "skummet" "do" "clean," "skummet" "compile"]
             "j" ["with-profile" "skummet" "do" "clean," "skummet" "jar"]})
