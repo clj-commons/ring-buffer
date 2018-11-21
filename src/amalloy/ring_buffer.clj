97b126db9b193fe70eb5f7ba839c2d1217ca7de8
Bruno Bonacci <bruno.bonacci@gmail.com>
2018-11-21 14:13:54 +0000

    fix Java11 ambiguous Collection.toArray definition

--- src/amalloy/ring_buffer.clj
+++ src/amalloy/ring_buffer.clj
@@ -71,7 +71,7 @@
     (.count this))
   (isEmpty [this]
     (empty? this))
-  (toArray [this dest]
+  (^objects toArray [this ^objects dest]
     (reduce (fn [idx item]
               (aset dest idx item)
               (inc idx))
