4d4483067d75e676b07cb0aadc037f34560c6ea6
danielcompton <desk@danielcompton.net>
2018-12-06 20:52:08 +1300

    Bump dependency versions and test against multiple JDKs

--- project.clj
+++ project.clj
@@ -1,16 +1,15 @@
-(defproject amalloy/ring-buffer "1.2.2"
+(defproject amalloy/ring-buffer "1.2.2-SNAPSHOT"
   :description "Persistent bounded-size queue implementation in Clojure"
-  :url "http://github.com/amalloy/ring-buffer"
+  :url "https://github.com/clj-commons/ring-buffer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.48"]]
-  :plugins [[lein-cljsbuild "1.0.6"]
-            [lein-doo "0.1.4"]]
-
-  :cljsbuild {
-    :builds {
-      :test {:source-paths ["src" "test"]
-             :compiler {:output-to "target/testable.js"
-                        :main 'amalloy.runner
-                        :optimizations :none}}}})
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/clojurescript "1.10.439"]]
+  :plugins [[lein-cljsbuild "1.1.7"]
+            [lein-doo "0.1.11"]]
+  :cljsbuild {:builds [{:id "test"
+                        :source-paths ["src" "test"]
+                        :compiler     {:output-to     "target/testable.js"
+                                       :main          amalloy.runner
+                                       :target :nodejs
+                                       :optimizations :none}}]})
