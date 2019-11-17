722a6a220fe770145372f344df45e30d5ecb1b72
Bruno Bonacci <bruno.bonacci@gmail.com>
2019-11-17 19:42:24 +0000

    setting clojure and clojurescript dependencies as provided

--- project.clj
+++ project.clj
@@ -3,16 +3,20 @@
   :url "https://github.com/clj-commons/ring-buffer"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]
-                 [org.clojure/clojurescript "1.10.439"]]
+  :dependencies [[org.clojure/clojure "1.10.1" :scope "provided"]
+                 [org.clojure/clojurescript "1.10.520" :scope "provided"]]
   :plugins [[lein-cljsbuild "1.1.7"]
             [lein-doo "0.1.11"]]
   :deploy-repositories [["releases" :clojars]
                         ["snapshots" :clojars]]
-  :profiles {:1.8 {:dependencies [[org.clojure/clojure "1.8.0"]]}
-             :1.9 {:dependencies [[org.clojure/clojure "1.9.0"]]}
-             :1.10.0 {:dependencies [[org.clojure/clojure "1.10.0"]]}
-             :1.10.1 {:dependencies [[org.clojure/clojure "1.10.1"]]}}
+  :profiles {:1.8    {:dependencies [[org.clojure/clojure "1.8.0"]
+                                     [org.clojure/clojurescript "1.8.51"]]}
+             :1.9    {:dependencies [[org.clojure/clojure "1.9.0"]
+                                     [org.clojure/clojurescript "1.9.946"]]}
+             :1.10.0 {:dependencies [[org.clojure/clojure "1.10.0"]
+                                     [org.clojure/clojurescript "1.10.520"]]}
+             :1.10.1 {:dependencies [[org.clojure/clojure "1.10.1"]
+                                     [org.clojure/clojurescript "1.10.520"]]}}
   :aliases {"test-all" ["with-profile" "+1.8:+1.9:+1.10.0:+1.10.1" "test"]
             "deps-all" ["with-profile" "+1.8:+1.9:+1.10.0:+1.10.1" "deps"]}
   :cljsbuild {:builds [{:id "node-test"
